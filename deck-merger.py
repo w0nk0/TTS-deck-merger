4f77edf93bd401836b02f9447e97fc45e0d71c57
W0nk0 The Sane <github@wonko.oib.com>
2015-01-08 03:47:08 +0100

    Fixed ssl error Suppressed urrlib security warning Copies imgur URL to clipboard Adds card 70

--- deck-merger.py
+++ deck-merger.py
@@ -1,18 +1,22 @@
 __author__ = 'nico'
 
-imgur_client = "faecd226b95e22c"
+imgur_client_id = "faecd226b95e22c"
 
 deck_image = "_deck.jpg"
 
-x_total = 5000
-y_total = 5000
+x_total = 6000
+y_total = 6000
 
-JPEG_QUALITY = 40
+JPEG_QUALITY = 30
 
 import os
 from PIL import Image
 import pyimgur
 
+# import requests
+from requests.packages.urllib3 import disable_warnings
+
+disable_warnings()
 
 def get_files(dir=".", pattern=".png"):
     files = [os.path.join(dir, x) for x in os.listdir(dir) if pattern in x]
@@ -28,6 +32,12 @@ def make_url_link(url):
         f.close()
 
 
+def card70(path):
+    cards = get_files(path, "70")
+    if len(cards):
+        if os.path.exists(cards[0]):
+            return cards[0]
+
 def main():
     path = raw_input("Directory to use ('.' = current -> just press Enter): ") or "."
     pattern = raw_input("Pattern of the card files (Enter -> '.png'): ") or ".png"
@@ -41,36 +51,44 @@ def main():
     x = y = 0
     x_size = x_total / 10
     y_size = y_total / 7
+    size_box = (x_size, y_size)
     for f in files:
         print ".",
         # print f
         cur = Image.open(f)
-        cur = cur.resize((x_size, y_size), Image.FLOYDSTEINBERG)
+        cur = cur.resize(size_box, Image.FLOYDSTEINBERG)
         i.paste(cur, (x, y))
         x += x_size
         if x >= x_total:
             x = 0
             y += y_size
+
+    # Add Card # 70
+    c70 = card70(path)
+    if c70:
+        c70img = Image.open(c70)
+        c70img = c70img.resize(size_box, Image.FLOYDSTEINBERG)
+        i.paste(c70img, (x_total - x_size, y_total - y_size))
+
     i.save(deck_image, format="JPEG", quality=JPEG_QUALITY, optimize=True, progressive=True)
-    # os.execl("Deck.jpg")
+    # i.save("_deck.png", format="PNG", optimize=True, progressive=True)
     try:
         print len(files), "cards in this deck!"
         print "initializing Imgur"
-        imgur = pyimgur.Imgur(imgur_client)
+        imgur = pyimgur.Imgur(imgur_client_id, verify=False)
         print "uploading...."
         uploaded = imgur.upload_image(deck_image, title="Tabletop Simulator deck")
         print "Success!"
         print "Image link:", uploaded.link
         make_url_link(uploaded.link)
-        # execfile(uploaded.permalink)
+        os.system("echo " + uploaded.link + " | clip")
+        print "..copied to clipboard!"
     except Exception, err:
         print err
         print "Something went wrong :/, probably imgur."
         print "\nThe image has been saved to %s though!" % deck_image
-    dummy = raw_input("\nDone. Please press Enter to exit.")
-    # execfile(deck_image)
 
-    pass
+    dummy = raw_input("\nDone. Please press Enter to exit.")
 
 
 if __name__ == "__main__":
